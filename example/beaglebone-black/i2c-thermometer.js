7c169b6690b2fbe46ccc9f91b5ed95fb9aa9128b
fivdi <bdscooke@gmail.com>
2019-09-21 13:36:04 +0200

    add mcp9808 i2c temperature sensor examples

---
+++ example/beaglebone-black/i2c-thermometer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const five = require('johnny-five');
+const TinyBeagleBoneIO = require('./tiny-beaglebone-io');
+
+const board = new five.Board({
+  io: new TinyBeagleBoneIO()
+});
+
+board.on('ready', function() {
+  var thermometer = new five.Thermometer({
+    controller: "MCP9808"
+  });
+
+  thermometer.on("change", function() {
+    console.log("celsius: %d", this.C);
+    console.log("fahrenheit: %d", this.F);
+    console.log("kelvin: %d", this.K);
+  });
+});
+
---
+++ example/raspberry-pi/i2c-thermometer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const five = require('johnny-five');
+const TinyRaspberryPiIO = require('./tiny-beaglebone-io');
+
+const board = new five.Board({
+  io: new TinyRaspberryPiIO()
+});
+
+board.on('ready', function() {
+  var thermometer = new five.Thermometer({
+    controller: "MCP9808"
+  });
+
+  thermometer.on("change", function() {
+    console.log("celsius: %d", this.C);
+    console.log("fahrenheit: %d", this.F);
+    console.log("kelvin: %d", this.K);
+  });
+});
+
