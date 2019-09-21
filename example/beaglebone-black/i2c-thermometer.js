e65ac92b1578aae0bbcbb67d75dd9178823a5eea
fivdi <bdscooke@gmail.com>
2019-09-21 13:46:24 +0200

    replace double quotes woth single quotes

--- example/beaglebone-black/i2c-thermometer.js
+++ example/beaglebone-black/i2c-thermometer.js
@@ -9,13 +9,13 @@ const board = new five.Board({
 
 board.on('ready', function() {
   const thermometer = new five.Thermometer({
-    controller: "MCP9808"
+    controller: 'MCP9808'
   });
 
-  thermometer.on("change", function() {
-    console.log("celsius: %d", this.C);
-    console.log("fahrenheit: %d", this.F);
-    console.log("kelvin: %d", this.K);
+  thermometer.on('change', function() {
+    console.log('celsius: %d', this.C);
+    console.log('fahrenheit: %d', this.F);
+    console.log('kelvin: %d', this.K);
   });
 });
 
